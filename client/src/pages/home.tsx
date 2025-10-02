44023c2874f7152642175caf2faedf5f518e2bfa
leatile mosimanyana <100219407+Cse21-034@users.noreply.github.com>
2025-10-02 14:47:57 +0200

    Update home.tsx

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,4 +1,4 @@
- import { Navigation } from "@/components/navigation";
+import { Navigation } from "@/components/navigation";
 import { Hero } from "@/components/hero";
 import { About } from "@/components/about";
 import { Timeline } from "@/components/timeline";
